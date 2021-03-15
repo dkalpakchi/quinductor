3107269acf9fde3d41ef39fac26185ce51399e69
Dmytro Kalpakchi <dmytroka@kth.se>
2021-03-15 13:13:29 +0100

    Added a shell script for generating files for auto-evaluation

---
+++ evaluate.sh
@@ -0,0 +1,8 @@
+python3 evaluate.py -l en -f data/tydiqa-v1.0-dev.jsonl -t templates/en/16132054753040054/ -r templates/en/ -pg pos_ngrams/en/ -ft tydiqa -cf
+python3 evaluate.py -l ar -f data/tydiqa-v1.0-dev.jsonl -t templates/ar/1614104416496133/ -r templates/ar/ -pg pos_ngrams/ar/ -ft tydiqa -cf
+python3 evaluate.py -l id -f data/tydiqa-v1.0-dev.jsonl -t templates/id/16140609246000547/ -r templates/id/ -pg pos_ngrams/id/ -ft tydiqa -cf
+python3 evaluate.py -l ja -f data/tydiqa-v1.0-dev.jsonl -t templates/ja/16140572221308537/ -r templates/ja/ -pg pos_ngrams/ja/ -ft tydiqa -rp -rtl
+python3 evaluate.py -l ko -f data/tydiqa-v1.0-dev.jsonl -t templates/ko/16140582210609627/ -r templates/ko/ -pg pos_ngrams/ko/ -ft tydiqa -rtl
+python3 evaluate.py -l te -f data/tydiqa-v1.0-dev.jsonl -t templates/te/16140691545631247/ -r templates/te/ -pg pos_ngrams/te/ -ft tydiqa -rtl
+python3 evaluate.py -l fi -f data/tydiqa-v1.0-dev.jsonl -t templates/fi/16132078825085254/ -r templates/fi/ -pg pos_ngrams/fi/ -ft tydiqa -cf
+python3 evaluate.py -l ru -f data/tydiqa-v1.0-dev.jsonl -t templates/ru/1613204358381249/ -r templates/ru/ -pg pos_ngrams/ru/ -ft tydiqa -cf -rd
